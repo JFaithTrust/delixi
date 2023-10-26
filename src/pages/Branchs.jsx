ebd16262ebfed2069d9408d39edaaa75144c17f6
JFaithTrust <jahongirsolijoniy@gmail.com>
2023-10-26 15:59:53 +0500

    add map

--- src/pages/Branchs.jsx
+++ src/pages/Branchs.jsx
@@ -1,18 +1,89 @@
 import React from "react";
-import { useTranslation } from "react-i18next";
+import { useMemo } from "react";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 
-const Branchs = () => {
-  const { t } = useTranslation();
+function Map() {
+  const center = useMemo(
+    () => ({ lat: 41.35582074386323, lng: 69.24943463933324 }),
+    []
+  );
 
   return (
     <div className="top-[78px] relative">
-      <div>
-        <h1>{t("greeting")}</h1>
-        <p>{t("welcome")}</p>
-        {/* AIzaSyAcfsk4C5rdqDe-TAtNFEQjcC6Vsak-zu4 */}
-      </div>
+      <GoogleMap
+        zoom={15}
+        center={center}
+        mapContainerClassName="map-container"
+      >
+        <Marker position={center} />
+      </GoogleMap>
     </div>
   );
+}
+
+const Branchs = () => {
+  // AIzaSyAcfsk4C5rdqDe-TAtNFEQjcC6Vsak-zu4
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: "AIzaSyAcfsk4C5rdqDe-TAtNFEQjcC6Vsak-zu4",
+  });
+
+  if (!isLoaded) return <div>Loading...</div>;
+  return <Map />;
 };
 
 export default Branchs;
+
+// import React from "react";
+// import { GoogleMap, LoadScript, OverlayView } from "@react-google-maps/api";
+
+// const Branchs = () => {
+//   const mapStyles = {
+//     width: "100%",
+//     height: "100vh",
+//   };
+
+//   const defaultCenter = {
+//     lat: 41.35582074386323,
+//     lng: 69.24943463933324,
+//   };
+
+//   const overlayPosition = {
+//     lat: 41.35582074386323,
+//     lng: 69.24943463933324,
+//   };
+
+//   const renderOverlayView = (props) => {
+//     return (
+//       <div
+//         style={{
+//           position: "absolute",
+//           width: "100px",
+//           height: "50px",
+//           backgroundColor: "red",
+//           borderRadius: "5px",
+//           padding: "5px",
+//         }}
+//       >
+//         Custom Overlay Content
+//       </div>
+//     );
+//   };
+
+//   return (
+//     <LoadScript googleMapsApiKey="AIzaSyAcfsk4C5rdqDe-TAtNFEQjcC6Vsak-zu4">
+//       <GoogleMap mapContainerStyle={mapStyles} zoom={17} center={defaultCenter}>
+//         <OverlayView
+//           position={overlayPosition}
+//           getPixelPositionOffset={(width, height) => ({
+//             x: -width / 2,
+//             y: -height,
+//           })}
+//         >
+//           {renderOverlayView}
+//         </OverlayView>
+//       </GoogleMap>
+//     </LoadScript>
+//   );
+// };
+
+// export default Branchs;
