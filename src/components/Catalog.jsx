d075c18f9e973aa96e8d20e3b0996e85b83ed314
JFaithTrust <95927694+JFaithTrust@users.noreply.github.com>
2023-11-20 15:18:33 +0500

    fixed bug

--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -32,10 +32,7 @@ const Catalog = () => {
         </Link>
       </div>
       <div className="flex 2xl:flex-row xl:flex-row flex-col justify-center 2xl:gap-20 xl:gap-20 gap-0">
-        <motion.div
-          variants={slideIn("left", "tween", 0.2, 1)}
-          className="flex 2xl:flex-col xl:flex-col lg:flex-row md:flex-row flex-col 2xl:space-y-4 xl:space-y-4 space-y-0 mt-20 2xl:w-[13vw] xl:w-[13vw] w-full justify-between 2xl:gap-y-0 xl:gap-y-0 lg:gap-y-0 md:gap-y-0 gap-y-3"
-        >
+        <div className="flex 2xl:flex-col xl:flex-col lg:flex-row md:flex-row flex-col 2xl:space-y-4 xl:space-y-4 space-y-0 mt-20 2xl:w-[13vw] xl:w-[13vw] w-full justify-between 2xl:gap-y-0 xl:gap-y-0 lg:gap-y-0 md:gap-y-0 gap-y-3">
           {categories.map((item) => (
             <div
               className="flex 2xl:flex-col xl:flex-col lg:flex-col md:flex-col flex-row border-2 rounded-md border-red-300 items-center py-2 px-4 cursor-pointer 2xl:justify-center xl:justify-center lg:justify-center md:justify-center justify-between 2xl:w-[13vw] xl:w-[13vw] lg:w-[13vw] md:w-[13vw] w-full"
@@ -58,16 +55,13 @@ const Catalog = () => {
               <h1 className="text-center font-semibold py-1.5">{item.name}</h1>
             </div>
           ))}
-        </motion.div>
-        <motion.div
-          variants={slideIn("right", "tween", 0.2, 1)}
-          className="border-2 border-red-300 rounded-md px-2 py-8 flex flex-col items-center text-center mt-20 space-y-3"
-        >
+        </div>
+        <div className="border-2 border-red-300 rounded-md px-2 py-8 flex flex-col items-center text-center mt-20 space-y-3">
           <h1 className="font-semibold text-xl text-red-600">
-          {t("cataloginfo")}
+            {t("cataloginfo")}
           </h1>
           <Product pro_id={id} />
-        </motion.div>
+        </div>
       </div>
     </div>
   );
